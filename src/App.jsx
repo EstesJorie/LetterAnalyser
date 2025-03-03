7eab60b732818b33f7b7871581c0a27ee387fdbd
Tim Aldred <tim5597@gmail.com>
2025-03-03 20:04:57 +0000

    I don't know

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React, { useState } from 'react'
 import TextGrabber from '../components/textbox'
 import ExampleAnalyser from '../components/exampleanalyser'
-<<<<<<< HEAD
+
 import Counts from '../components/counts'
 
-function App() {
-
-  const [text, setText] = useState('');
-
-  return (
-    <>
-      <Counts />
-=======
 
 function App() {
   const [text, setText] = useState('');
@@ -19,9 +11,9 @@ function App() {
   return (
     <div className='bggradient text-white w-screen h-screen px-35 py-5'>
       <div className='mainbox w-full h-full'>
->>>>>>> e515f1f360404f027ca1e8e9a61f27ac8057440a
       <TextGrabber setText={setText} />
       <ExampleAnalyser text={text} />
+      <Counts />
       <input></input>
       </div>
     </div>
