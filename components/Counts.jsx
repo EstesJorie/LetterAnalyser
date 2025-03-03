b817690b2d532cda934d016b17ea254c93e3c092
Tom Parkinson <tomparkinson12@gmail.com>
2025-03-03 18:53:31 +0000

    commit

---
+++ components/Counts.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function Counts(input={fakeinput}) {
+    const [characterCount, setCharacterCount] = useState(0)
+    const [wordCount, setWordCount] = useState(0)
+    const [sentenceCount, setSentenceCount] = useState(0)
+
+    const input = "Hello, is this thing working. How many sentences are in here."
+
+    let length = input.length
+    let spaceCount = input.split(" ").length - 1
+
+    let stopCount = input.split(".").length - 1
+
+    const Update = () => {
+    setCharacterCount(length - spaceCount)
+    setWordCount(spaceCount + stopCount)
+    setSentenceCount(stopCount)
+    }
+    
+
+    console.log(characterCount, wordCount, sentenceCount)
+    
+  return (
+    <div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
