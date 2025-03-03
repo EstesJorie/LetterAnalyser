496fc2c23d633d1308568a4d35940e6940336ad7
JosephB2 <josephbousfield99@gmail.com>
2025-03-03 19:28:09 +0000

    Add background gradient and main content box

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import TextGrabber from '../components/textbox'
 import ExampleAnalyser from '../components/exampleanalyser'
@@ -8,22 +7,13 @@ function App() {
   const [text, setText] = useState('');
 
   return (
-    <>
+    <div className='bggradient text-white w-screen h-screen px-35 py-5'>
+      <div className='mainbox w-full h-full'>
       <TextGrabber setText={setText} />
       <ExampleAnalyser text={text} />
-=======
-import { useState } from 'react'
-import Counts from '../components/counts'
-
-function App() {
-
-
-  return (
-    <>
-      <Counts />
->>>>>>> 645dd5ace4d7f792e06f2bc44012b1f6b4cb8e7e
       <input></input>
-    </>
+      </div>
+    </div>
   )
 }
 
