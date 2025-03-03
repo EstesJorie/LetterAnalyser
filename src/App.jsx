d92938243ce8ff446496a66115e6c14e4133ed4a
Tim Aldred <tim5597@gmail.com>
2025-03-03 19:10:03 +0000

    text grabber ii

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import TextGrabber from '../components/textbox'
+import ExampleAnalyser from '../components/exampleanalyser'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [text, setText] = useState('');
 
   return (
     <>
+      <TextGrabber setText={setText} />
+      <ExampleAnalyser text={text} />
       <input></input>
     </>
   )
