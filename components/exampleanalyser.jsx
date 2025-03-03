6bbfd01f7f28acb870cfe8f9882cd78e0cba2cf3
Tim Aldred <tim5597@gmail.com>
2025-03-03 19:27:22 +0000

    Counts

--- components/exampleanalyser.jsx
+++ components/exampleanalyser.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ExampleAnalyser({ text }) {
     return (
         <div>
-            <p>{text || "no text"}</p>
+            <p>{text}</p>
         </div>
     )
-}
+}   
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import TextGrabber from '../components/textbox'
 import ExampleAnalyser from '../components/exampleanalyser'
-
-function App() {
-  const [count, setCount] = useState(0)
-  const [text, setText] = useState('');
-
-  return (
-    <>
-      <TextGrabber setText={setText} />
-      <ExampleAnalyser text={text} />
-=======
-import { useState } from 'react'
 import Counts from '../components/counts'
 
 function App() {
 
+  const [text, setText] = useState('');
 
   return (
     <>
       <Counts />
->>>>>>> 645dd5ace4d7f792e06f2bc44012b1f6b4cb8e7e
+      <TextGrabber setText={setText} />
+      <ExampleAnalyser text={text} />
       <input></input>
     </>
   )
