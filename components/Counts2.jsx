a5fa931223cbb4a6cebc9ff3e4c7ba644aa1638d
Tim Aldred <tim5597@gmail.com>
2025-03-03 19:37:21 +0000

    Counts2 test

--- components/Counts2.jsx
+++ components/Counts2.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import { useState } from 'react'
 
 export default function Counts2({text}) {
-
-
     let length = text.length
 
-    
   return (
     <>
-
         <div className='bg-blue-500 w-full'>{length}</div>
-        <div></div>
-        <div></div>
-
     </>
   )
 }
