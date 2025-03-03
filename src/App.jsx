0581a9c7f91da30c6389ab836c09d11a34b31d4f
EstesJorie <joe.tresise@aol.com>
2025-03-03 19:23:15 +0000

    030325 Console logging for textbox.jsx and change to App.jsx
    Addition of a console.log to textbox.jsx to log text in console for
    passing to other components. Updates to App.jsx to allow correct page
    rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import TextGrabber from '../components/textbox'
 import ExampleAnalyser from '../components/exampleanalyser'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [text, setText] = useState('');
 
   return (
     <>
       <TextGrabber setText={setText} />
       <ExampleAnalyser text={text} />
-=======
-import { useState } from 'react'
-import Counts from '../components/counts'
-
-function App() {
-
-
-  return (
-    <>
-      <Counts />
->>>>>>> 645dd5ace4d7f792e06f2bc44012b1f6b4cb8e7e
-      <input></input>
     </>
-  )
+  );
 }
-
 export default App
