886648216c97cfe41771ad00843aaeeed5bc8784
Tim Aldred <tim5597@gmail.com>
2025-03-06 19:12:11 +0000

    change from "button" to "text area updates" submit

--- src/components/textbox.jsx
+++ src/components/textbox.jsx
@@ -1,16 +1,8 @@
 import React, {useState} from 'react'
 
 export default function TextGrabber({ setText }) {
-
-    function handleFormSubmit(event){
-            event.preventDefault()
-
-            let formData = new FormData(event.target)
-            let data = Object.fromEntries(formData.entries())
-
-            setText(data.text); 
-
-                console.log(data)
+        function handleFormSubmit(event) {
+            setText(event.target.value);
         }
 
 return (
@@ -18,9 +10,7 @@ return (
         <div>
         <form onSubmit={handleFormSubmit} action="" id="text-form">
             <textarea rows="10" name="text" placeholder='Start typing...'
-             className="w-full p-3 mono rounded-xs text-white bg-[#284b63] placeholder-[#14213d]"></textarea>
-            <br />
-            <button className='scifi'> Analyse </button>
+             className="w-full p-3 mono rounded-xs text-white bg-[#284b63] placeholder-[#14213d]" onChange={handleFormSubmit} ></textarea>
         </form>
         </div>
         </>
