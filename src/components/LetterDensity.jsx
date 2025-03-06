cef3fed47ca403e32e716d010e89793f4047d5c9
Tom Parkinson <tomparkinson12@gmail.com>
2025-03-06 19:22:58 +0000

    bars

--- src/components/LetterDensity.jsx
+++ src/components/LetterDensity.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Bars from './Bars';
 
 const LetterDensity = ({ text }) => {
     const [textValue, setTextValue] = useState("");
@@ -42,6 +43,18 @@ const LetterDensity = ({ text }) => {
                         <td>{letter}</td>
                         <td className="pr-4">{count}</td>
                         <td>{((count / letterCount) * 100).toFixed(2)}%</td>
+                        <td className='w-full'><div className="w-full  rounded-full h-4 flex items-center">
+                              <div
+                                className="bg-green-500 h-4 rounded-full transition-all duration-500 ease-in-out flex items-center justify-end pr-1"
+                                style={{ width: `${(count/letterCount)*100}%` }}
+                              >
+                                {(count/letterCount)*100 > 5 && (
+                                  <span className="text-xs text-white font-semibold">
+                                    {((count/letterCount)*100).toFixed(0)}%
+                                  </span>
+                                )}
+                              </div>
+                            </div></td>
                       </tr>
                     ))}
                   </tbody>
