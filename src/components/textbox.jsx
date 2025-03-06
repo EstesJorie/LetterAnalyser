4d1aeb709756b164f53b8f25e0395c89c5ee00cb
JosephB2 <josephbousfield99@gmail.com>
2025-03-06 19:01:23 +0000

    Update bg and placeholder styles in textbox.jsx

--- src/components/textbox.jsx
+++ src/components/textbox.jsx
@@ -17,7 +17,8 @@ return (
         <>
         <div>
         <form onSubmit={handleFormSubmit} action="" id="text-form">
-            <textarea rows="10" name="text" className="w-full p-3 text-white bg-blue-950"></textarea>
+            <textarea rows="10" name="text" placeholder='Start typing...'
+             className="w-full p-3 mono rounded-xs text-white bg-[#284b63] placeholder-[#14213d]"></textarea>
             <br />
             <button className='scifi'> Analyse </button>
         </form>
