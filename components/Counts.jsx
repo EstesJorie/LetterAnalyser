69ea0029a84167c21d8da412082159146270b42c
Tom Parkinson <tomparkinson12@gmail.com>
2025-03-06 18:15:54 +0000

    counts styling

--- components/Counts.jsx
+++ components/Counts.jsx
@@ -25,20 +25,20 @@ export default function Counts({text}) {
     
   return (
     <>
-        <div className='flex items-center justify-center gap-5 p-4'>
-            <div className='bg-[#D4A1FB] w-1/3 h-20 rounded flex-col text-white'>
-                <span className='text-2xl'>{characterCount}</span>
-                <span className='align-bottom'>Character Count</span>
+        <div className='flex gap-5 p-4 '>
+            <div className='grid bg-[#D4A1FB] w-1/3 h-30 rounded-lg text-black place-items-center font-bold'>
+                <span className='text-6xl'>{characterCount}</span>
+                <span className='text-2xl align-bottom'>Character Count</span>
             </div>
 
-            <div className='bg-[#FFA001] w-1/3 h-20 rounded flex-col text-white'>
-                <span className='text-2xl'>{wordCount}</span>
-                <span className='align-bottom'>Word Count</span>
+            <div className='grid bg-[#FFA001] w-1/3 h-30 rounded-lg text-black place-items-center font-bold'>
+                <span className='text-6xl'>{wordCount}</span>
+                <span className='text-2xl align-bottom'>Word Count</span>
             </div>
             
-            <div className='bg-[#FE8159] w-1/3 h-20 rounded flex-col text-white'>
-                <span className='text-2xl justify-center'>{sentenceCount}</span>
-                <span className='align-bottom'>Sentence Count</span>
+            <div className='grid bg-[#FE8159] w-1/3 h-30 rounded-lg text-black place-items-center font-bold'>
+                <span className='text-6xl'>{sentenceCount}</span>
+                <span className='text-2xl '>Sentence Count</span>
             </div>
         </div>
     </>
