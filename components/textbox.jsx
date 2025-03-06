604185ad02da4bc9d33d4cad26c52be4c24cc992
Tim Aldred <tim5597@gmail.com>
2025-03-06 18:39:58 +0000

    bigger text box

--- components/textbox.jsx
+++ components/textbox.jsx
@@ -17,15 +17,10 @@ return (
         <>
         <div>
         <form onSubmit={handleFormSubmit} action="" id="text-form">
-<<<<<<< HEAD
-            <textarea rows="10" name="text"
+            <textarea rows="10" name="text" 
               className="w-full p-3 text-white bg-blue-900 "/>
             <br />
             <button> Analyse </button>
-=======
-            <input type="text" name="text" id="" className="mono" placeholder="Type here..." />
-            <button className='scifi'> Analyse </button>
->>>>>>> fd1b59e53c69a72cea578d58b5d08c309532956f
         </form>
         </div>
         </>
