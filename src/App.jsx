c057bbe673c9b7bda9366c6515856ab6acfb3e18
EstesJorie <joe.tresise@aol.com>
2025-03-06 17:57:23 +0000

    something

--- src/App.jsx
+++ src/App.jsx
@@ -12,25 +12,12 @@ function App() {
 
   return (
     <>
-    <div className='bggradient text-white w-screen h-screen px-35 py-5'>
-<<<<<<< HEAD
-      <div className='mainbox w-full h-full'>
-<<<<<<< HEAD
-      <TextGrabber setText={setText} />
-      <ExampleAnalyser text={text} />
-      <Counts />
-      <input></input>
-=======
-=======
       <div className='mainbox w-full h-full p-5'>
->>>>>>> 689c1729d142abfe79f3e9de727bcbdac503584b
         <TextGrabber setText={setText} />
         <ExampleAnalyser text={text} />
         <Counts text={text}/>
         <LetterDensity text={text} />
->>>>>>> a707e45d372f26938c4b42bff36fd2cfe4e565b7
       </div>
-    </div>
     </>
   );
 }
