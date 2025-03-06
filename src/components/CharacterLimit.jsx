ad8310f1483dd1c9642591737fd48ea3dd11a6e1
EstesJorie <joe.tresise@aol.com>
2025-03-06 19:34:34 +0000

    060325 changes to website layout

--- src/components/CharacterLimit.jsx
+++ src/components/CharacterLimit.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useStore } from '../App'; // Adjust the import path as necessary
+import { useStore } from './TextAnalyser'; // Adjust the import path as necessary
 
 export default function CharacterLimit() {
     const { text } = useStore();
