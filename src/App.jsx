7b97ba6673e0fabf57a8a0850a0cfcd9d5bc3309
Tim Aldred <tim5597@gmail.com>
2025-03-06 18:23:45 +0000

    add reading time

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Counts from '../components/counts';
 import React, { useState } from 'react';
 import TextGrabber from '../components/textbox';
 import LetterDensity from '../components/LetterDensity';
-import ReadingTime from '../components/readingtime';
+import ReadingTime from '../components/ReadingTime';
 
 
 function App() {
@@ -13,7 +13,6 @@ function App() {
       <div className='bggradient text-white w-screen h-screen px-35 py-5'>
         <div className='mainbox w-full h-full p-5'>
           <TextGrabber setText={setText} />
-          <ExampleAnalyser text={text} />
           <Counts text={text}/>
           <ReadingTime text={text} />
           <LetterDensity text={text} />
