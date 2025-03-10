afa88e52f792bcee36562de79b67e165012d5189
EstesJorie <joe.tresise@aol.com>
2025-03-10 20:10:13 +0000

    100325 changes to character limit
    display the character limit and the total characters out of the limit

--- src/components/TimCharLimit.jsx
+++ src/components/TimCharLimit.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 export default function TimCharLimit({ text }) {
     const [isCharLimitActive, setIsCharLimitActive] = useState(false);
     const [labelText, setLabelText] = useState('Set Character Limit');
-    const maxCharLimit = 250;
+    const maxCharLimit = 100;
 
     const handleCheckboxChange = () => {
         setIsCharLimitActive(!isCharLimitActive);
@@ -41,6 +41,11 @@ export default function TimCharLimit({ text }) {
                     `}>
                     {labelText}
                 </label>
+                {isCharLimitActive && (
+                    <div className="text-gray-400 text-md pt-2  ml-4">
+                        {text.length}/{maxCharLimit}
+                    </div>
+                )}
             </div>
         </div>
     );
